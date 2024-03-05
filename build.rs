edefa52ff7e196448aeb3904fed902f82ce46c4f
Piuro <95382758+yPiuro@users.noreply.github.com>
2024-03-06 10:22:29 +1300

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
-extern crate winresource;
-
 fn main() {
   if cfg!(target_os = "windows") {
+    extern crate winresource;
     let mut res = winresource::WindowsResource::new();
     res.set_icon("icon.ico");
     res.compile().unwrap();
