6914a87962e8455e4f181a808d1c2cae5f0cac6d
Piuro <95382758+yPiuro@users.noreply.github.com>
2024-03-06 10:59:51 +1300

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 fn main() {
   if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
-    extern crate winresource;
     let mut res = winresource::WindowsResource::new();
     res.set_icon("icon.ico");
     res.compile().unwrap();
